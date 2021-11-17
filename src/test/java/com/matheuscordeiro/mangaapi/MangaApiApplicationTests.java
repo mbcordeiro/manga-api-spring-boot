cce2ec86769290d736c1922041ed185ca36d482d
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-11-16 23:37:55 -0300

    First commit

---
+++ src/test/java/com/matheuscordeiro/mangaapi/MangaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.mangaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
